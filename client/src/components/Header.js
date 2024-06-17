7ab218c0a9225ba78a88147ee133c5c45645afdc
bhargavdash <125730661+bhargavdash@users.noreply.github.com>
2024-06-17 18:31:08 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import {UserContext} from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:4000/profile', {
+    fetch('https://blog-app-2w2i.onrender.com/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
